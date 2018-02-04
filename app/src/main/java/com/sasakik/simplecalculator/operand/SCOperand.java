56b9dd5f6f2efe6366f602440ce739c6bbc9a2db
sasakik <mega_red@mac.com>
2018-02-04 22:08:21 +0900

    本処理を実装

--- app/src/main/java/com/sasakik/simplecalculator/operand/SCOperand.java
+++ app/src/main/java/com/sasakik/simplecalculator/operand/SCOperand.java
@@ -21,23 +21,41 @@ public class SCOperand {
 
     public static final String TAG = "SCOperand";
 
-    /* データ値本体 */
+    /**
+     * データ値本体
+     */
     protected BigDecimal mValue;
+
+    /**
+     * 次回"."付与フラグ
+     */
+    protected boolean mContainsNextDot;
 
     public SCOperand() {
         mValue = new BigDecimal(0);
+        mContainsNextDot = false;
     }
 
     public SCOperand(String inputsource) {
-        update(inputsource);
+        try {
+            mValue = new BigDecimal(inputsource);
+            if (inputsource.endsWith(".")) {
+                mContainsNextDot = true;
+            }
+        } catch (NumberFormatException e) {
+            mValue = new BigDecimal(0);
+            Log.e(TAG, "constructor failed. invalid inputsource=" + inputsource);
+        }
     }
 
     public SCOperand(BigDecimal input) {
         mValue = input;
+        mContainsNextDot = false;
     }
 
     public SCOperand(int input) {
         mValue = new BigDecimal(input);
+        mContainsNextDot = false;
     }
 
     public double getdouble() {
@@ -46,6 +64,76 @@ public class SCOperand {
 
     public BigDecimal getValue() {
         return mValue;
+    }
+
+    /**
+     * 次回"."付与フラグ返却
+     *
+     * @return
+     */
+    public boolean containsNextDot() {
+        return mContainsNextDot;
+    }
+
+    /**
+     * "."の二重要求にならないかの判定<BR>
+     * 次回"."付与フラグがONか、もしくはすでに小数点が<BR>
+     * 含まれている状態で"."が要求されるとNGと判定する
+     *
+     * @param input 更新要求値
+     * @return true：二重要求になっている／false：なっていない
+     */
+    public boolean isDotOverLaps(String input) {
+        if (input != null && !input.isEmpty()) {
+            if (mContainsNextDot && input.contains(".")) {
+                return true;
+            }
+            if (isValueContainsDot() && input.contains(".")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 現在のデータ値が小数点含みか否かを返却
+     *
+     * @return true：含まれている／false：含まれていない
+     */
+    public boolean isValueContainsDot() {
+        return (mValue.scale() > 0);
+    }
+
+    /**
+     * データ値更新
+     *
+     * @param input 更新要求値
+     */
+    public void update(String input) {
+        String parameter = input;
+        /* "."が重複するケースは扱いが大変になるので無視 */
+        if (!isDotOverLaps(input)) {
+            if (mValue != null) {
+                if (mContainsNextDot) {
+                    parameter = mValue.toString() + "." + input;
+                    mContainsNextDot = false;
+                } else {
+                    parameter = mValue.toString() + input;
+                }
+            }
+
+            /* "."のみである場合は"."を付与 */
+            if (!mContainsNextDot && input.equals(".") && !isValueContainsDot()) {
+                mContainsNextDot = true;
+            }
+
+            try {
+                mValue = new BigDecimal(parameter);
+            } catch (NumberFormatException e) {
+                mValue = new BigDecimal(0);
+                Log.e(TAG, "update failed. invalid parameter=" + parameter);
+            }
+        }
     }
 
     public String toString() {
@@ -61,18 +149,5 @@ public class SCOperand {
         }
 
         return false;
-    }
-
-    public void update(String input) {
-        String parameter = input;
-        if (mValue != null) {
-            parameter = mValue.toString() + input;
-        }
-        try {
-            mValue = new BigDecimal(input);
-        } catch (NumberFormatException e) {
-            mValue = new BigDecimal(0);
-            Log.e(TAG, "constructor failed. invalid parameter=" + parameter);
-        }
     }
 }
